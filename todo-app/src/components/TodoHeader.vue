3aa859a530cd8a6ef189fcbffbd3f4c2fae5e729
stories282 <stories282@gmail.com>
2020-02-21 17:29:16 +0900

    앱 스타일, 제목 스타일 적용

--- todo-app/src/components/TodoHeader.vue
+++ todo-app/src/components/TodoHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>TodoHeader</div>
+    <h1>TODO IT</h1>
 </template>
 
 <script>
@@ -8,4 +8,9 @@ export default {
 </script>
 
 <style>
+    h1 {
+        color: #2F3B52;
+        font-weight: 900;
+        margin: 2.5rem 0 1.5rem;
+    }
 </style>
