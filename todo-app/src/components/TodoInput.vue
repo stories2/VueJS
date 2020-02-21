cd32c814a8d56469984b0a6f277e573df07d8b23
stories282 <stories282@gmail.com>
2020-02-21 17:49:16 +0900

    입력창 디자인

--- todo-app/src/components/TodoInput.vue
+++ todo-app/src/components/TodoInput.vue
@@ -1,11 +1,63 @@
 <template>
-    <div>TodoInput</div>
+    <div class="inputBox shadow">
+        <input type="text" v-model="newTodoItem" placeholder="Type it">
+        <span class="addContainer" v-on:click="addTodo">
+            <font-awesome-icon class="addBtn" icon="plus" />
+                <!-- <font-awesome-icon icon="user-secret" /> -->
+        </span>
+    </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            newTodoItem: ''
+        }
+    },
+    methods: {
+        addTodo() {
+
+            if (this.newTodoItem !== "") {
+                const value = this.newTodoItem && this.newTodoItem.trim();
+                localStorage.setItem(value, value);
+                console.log(`NEW TODO ${this.newTodoItem}, ${value}`);
+                this.clearInput();
+            }
+        },
+        clearInput() {
+            this.newTodoItem = '';
+        }
+    }
 }
 </script>
 
 <style>
+    input:focus {
+        outline: none;
+    }
+    .inputBox {
+        background: white;
+        height: 50px;
+        line-height: 50px;
+        border-radius: 5px;
+    }
+    
+    .inputBox input {
+        border-style: none;
+        font-size: 0.9rem;
+    }
+
+    .addContainer {
+        float: right;
+        background: linear-gradient(to right, #6478FB, #8763FB);
+        display: block;
+        width: 3rem;
+        border-radius: 0 5px 5px 0;
+    }
+
+    .addBtn {
+        color: white;
+        vertical-align: middle;
+    }
 </style>
--- todo-app/src/main.js
+++ todo-app/src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faUserSecret, faPlus } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(faUserSecret)
+library.add(faPlus)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
